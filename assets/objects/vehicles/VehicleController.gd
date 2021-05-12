0e4702892fa2b4f2e6f09c3382ec2d4324ef78d1
MikaelSantilio <mikael.santilio@gmail.com>
2021-05-12 20:48:51 -0300

    added level01 prototype

--- assets/objects/vehicles/VehicleController.gd
+++ assets/objects/vehicles/VehicleController.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 export var SPEED = 8
+export var CAN_ROTATE = true
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
@@ -10,3 +11,9 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("ui_left"):
 		$r_wheel.angular_velocity -= SPEED
 		$l_wheel.angular_velocity -= SPEED
+	
+	elif CAN_ROTATE and Input.is_action_pressed("ui_up"):
+		rotation_degrees -= 0.5
+	
+	elif CAN_ROTATE and Input.is_action_pressed("ui_down"):
+		rotation_degrees += 0.5
