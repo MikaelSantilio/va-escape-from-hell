2741b03025e78f268c2fd0b8da57409657bc2be8
MikaelSantilio <mikael.santilio@gmail.com>
2021-05-15 21:59:36 -0300

    added level final shape

--- scenes/levels/Level01.gd
+++ scenes/levels/Level01.gd
@@ -4,7 +4,7 @@ var Settings = load("res://assets/objects/Settings.gd")
 
 func _ready():
 	var settings = Settings.new()
-	var vehicle_settings = settings.players["police_car"]
+	var vehicle_settings = settings.players["monster"]
 	var vehicle = load(vehicle_settings.path).instance()
 	vehicle.position = Vector2(18200, 860)
 	var camera = vehicle.get_node("Camera2D")
